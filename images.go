3b06c53fd978b7d1d228e6e62bd9b88615183237
Sam Reis <sreis@atlassian.com>
2014-08-13 10:53:34 +1000

    images: add gained size to label

--- images.go
+++ images.go
@@ -184,9 +184,9 @@ func jsonToDot(images *[]Image) string {
 			buffer.WriteString(fmt.Sprintf(" \"%s\" -> \"%s\"\n", truncate(image.ParentId), truncate(image.Id)))
 		}
 		if image.RepoTags[0] != "<none>:<none>" {
-			buffer.WriteString(fmt.Sprintf(" \"%s\" [label=\"%s (%s)\\n%s\",shape=box,fillcolor=\"paleturquoise\",style=\"filled,rounded\"];\n", truncate(image.Id), truncate(image.Id), humanSize(image.VirtualSize), strings.Join(image.RepoTags, "\\n")))
+			buffer.WriteString(fmt.Sprintf(" \"%s\" [label=\"%s (+%s) (%s)\\n%s\",shape=box,fillcolor=\"paleturquoise\",style=\"filled,rounded\"];\n", truncate(image.Id), truncate(image.Id), humanSize(image.Size), humanSize(image.VirtualSize), strings.Join(image.RepoTags, "\\n")))
 		} else {
-			buffer.WriteString(fmt.Sprintf(" \"%s\" [label=\"%s (%s)\"];\n", truncate(image.Id), truncate(image.Id), humanSize(image.VirtualSize)))
+			buffer.WriteString(fmt.Sprintf(" \"%s\" [label=\"%s (+%s) (%s)\"];\n", truncate(image.Id), truncate(image.Id), humanSize(image.Size), humanSize(image.VirtualSize)))
     }
 	}
 
